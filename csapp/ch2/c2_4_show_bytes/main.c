c8f47a9cd66b838dd79eb59752b9576a3ceff1df
gangcheng <gangcheng1030@gmail.com>
2021-06-28 23:25:02 +0800

    show bytes

---
+++ csapp/ch2/c2_4_show_bytes/main.c
@@ -0,0 +1,30 @@
+//
+// Created by gang cheng on 2021/6/28.
+//
+
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; ++i) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main() {
+    int x = 12345;
+    show_int(x);
+    int *p = &x;
+    show_pointer(p);
+}
